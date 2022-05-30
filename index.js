8005383b8c8b6cb03d1f23224026f6c78dce6a67
igordvelasco <66185381+igordvelasco@users.noreply.github.com>
2022-05-30 12:30:01 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
-app.get(['/facebook', '/instagram', '/whatsapp'], function (req, res) {
+app.get(['/whatsapp'], function (req, res) {
   if (
     req.query['hub.mode'] == 'subscribe' &&
     req.query['hub.verify_token'] == token
@@ -33,67 +33,41 @@ app.get(['/facebook', '/instagram', '/whatsapp'], function (req, res) {
   }
 });
 
-app.post('/facebook', function (req, res) {
-  console.log('Facebook request body:', req.body);
-
-  if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
-    res.sendStatus(401);
-    return;
-  }
-
-  console.log('request header X-Hub-Signature validated');
-  // Process the Facebook updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
 app.post('/whatsapp', async function (req, res) {
-  if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
-    res.sendStatus(401);
-    return;
-  }
+    if (!req.isXHubValid()) {
+      console.log('Warning - request header X-Hub-Signature not present or invalid');
+      res.sendStatus(401);
+      return;
+    }
 
   console.log('request header X-Hub-Signature validated');
 
-  const message = req.body.entry[0].changes[0].value.messages[0].text.body;
-  const phone = req.body.entry[0].changes[0].value.messages[0].from;
+  const message = req.body[0].entry[0].changes[0].value.messages[0].text.body;
+  const phone = req.body[0].entry[0].changes[0].value.messages[0].from;
 
   let template;
 
   switch (message) {
     case 'Conhecer os projetos':
+      template = 'projetos'
+      break
     case 'Problemas com plataforma':
+
     case 'Formação de professores':
+
     default:
       template = 'boas_vindas'
   }
-  
+ 
   await fetch('https://graph.facebook.com/v13.0/100679882671832/messages', {
     method: 'post',
-    body: JSON.stringify({
-      "messaging_product": "whatsapp",
-      "to": phone,
-      "type": "template",
-      "template": {
-        "name": template,
-        "language": { "code": "pt_BR" }
-      }
-    }),
+    body: "{ \"messaging_product\": \"whatsapp\", \"to\": \""+phone+"\", \"type\": \"template\", \"template\": { \"name\": \""+template+"\", \"language\": { \"code\": \"pt_BR\" } } }",
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${jwt}`
     }
   });
 
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
-app.post('/intagram', function (req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
   received_updates.unshift(req.body);
   res.sendStatus(200);
 });
