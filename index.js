8b1a522aa9bf326793e2e06deee8583aa7e500c5
igordvelasco <66185381+igordvelasco@users.noreply.github.com>
2022-05-30 13:12:38 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -43,8 +43,14 @@ app.post('/whatsapp', async function (req, res) {
     // console.log('request header X-Hub-Signature validated');
     // }
 
-  const message = req.body[0].entry[0].changes[0].value.messages[0].text.body;
-  const phone = req.body[0].entry[0].changes[0].value.messages[0].from;
+  const[data] = body
+  const{entry} = data
+
+  if(!entry)
+    return res.send("Fudeu!")
+
+  const message = entry[0].changes[0].value.messages[0].text.body;
+  const phone = entry[0].changes[0].value.messages[0].from;
 
   let template;
 
