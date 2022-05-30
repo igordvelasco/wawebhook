fc7a61f2b23c749691f8dcd64feecc84f35fa0a0
igordvelasco <66185381+igordvelasco@users.noreply.github.com>
2022-05-30 12:42:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -17,59 +17,62 @@ app.use(bodyParser.json());
 const token = process.env.TOKEN || 'token';
 const received_updates = [];
 
-app.get('/', function (req, res) {
-  console.log(req);
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
-});
 
-app.get(['/whatsapp'], function (req, res) {
-  if (
-    req.query['hub.mode'] == 'subscribe' &&
-    req.query['hub.verify_token'] == token
-  ) {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.sendStatus(400);
-  }
-});
+app.get('/', (req, res) => res.send('esta caralha esta funcionando com a buceta do token ' + jwt))
 
-app.post('/whatsapp', async function (req, res) {
-    // if (!req.isXHubValid()) {
-    //   console.log('Warning - request header X-Hub-Signature not present or invalid');
-    //   res.sendStatus(401);
-    //   return;
-    // }
+// app.get('/', function (req, res) {
+//   console.log(req);
+//   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+// });
 
-  console.log('request header X-Hub-Signature validated');
+// app.get(['/whatsapp'], function (req, res) {
+//   if (
+//     req.query['hub.mode'] == 'subscribe' &&
+//     req.query['hub.verify_token'] == token
+//   ) {
+//     res.send(req.query['hub.challenge']);
+//   } else {
+//     res.sendStatus(400);
+//   }
+// });
 
-  const message = req.body[0].entry[0].changes[0].value.messages[0].text.body;
-  const phone = req.body[0].entry[0].changes[0].value.messages[0].from;
+// app.post('/whatsapp', async function (req, res) {
+//     // if (!req.isXHubValid()) {
+//     //   console.log('Warning - request header X-Hub-Signature not present or invalid');
+//     //   res.sendStatus(401);
+//     //   return;
+//     // }
 
-  let template;
+//   console.log('request header X-Hub-Signature validated');
 
-  switch (message) {
-    case 'Conhecer os projetos':
-      template = 'projetos'
-      break
-    case 'Problemas com plataforma':
+//   const message = req.body[0].entry[0].changes[0].value.messages[0].text.body;
+//   const phone = req.body[0].entry[0].changes[0].value.messages[0].from;
 
-    case 'Formação de professores':
+//   let template;
 
-    default:
-      template = 'boas_vindas'
-  }
+//   switch (message) {
+//     case 'Conhecer os projetos':
+//       template = 'projetos'
+//       break
+//     case 'Problemas com plataforma':
+
+//     case 'Formação de professores':
+
+//     default:
+//       template = 'boas_vindas'
+//   }
  
-  await fetch('https://graph.facebook.com/v13.0/100679882671832/messages', {
-    method: 'post',
-    body: "{ \"messaging_product\": \"whatsapp\", \"to\": \""+phone+"\", \"type\": \"template\", \"template\": { \"name\": \""+template+"\", \"language\": { \"code\": \"pt_BR\" } } }",
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${jwt}`
-    }
-  });
+//   await fetch('https://graph.facebook.com/v13.0/100679882671832/messages', {
+//     method: 'post',
+//     body: "{ \"messaging_product\": \"whatsapp\", \"to\": \""+phone+"\", \"type\": \"template\", \"template\": { \"name\": \""+template+"\", \"language\": { \"code\": \"pt_BR\" } } }",
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization': `Bearer ${jwt}`
+//     }
+//   });
 
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
+//   received_updates.unshift(req.body);
+//   res.sendStatus(200);
+// });
 
 app.listen();
