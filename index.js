2bdb8c4ff80435be09b7b6992b65c808141aa0d0
igordvelasco <66185381+igordvelasco@users.noreply.github.com>
2022-05-30 12:34:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -34,11 +34,11 @@ app.get(['/whatsapp'], function (req, res) {
 });
 
 app.post('/whatsapp', async function (req, res) {
-    if (!req.isXHubValid()) {
-      console.log('Warning - request header X-Hub-Signature not present or invalid');
-      res.sendStatus(401);
-      return;
-    }
+    // if (!req.isXHubValid()) {
+    //   console.log('Warning - request header X-Hub-Signature not present or invalid');
+    //   res.sendStatus(401);
+    //   return;
+    // }
 
   console.log('request header X-Hub-Signature validated');
 
